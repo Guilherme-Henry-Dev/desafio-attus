57483c8b27706acf6b4be392075775996280a998
Guilherme Henry <guilherme.henrydesigner@gmail.com>
2026-04-14 14:42:34 -0300

    feat: solve change detection and rxjs subscription issues

---
+++ src/app/challenges/reactivity/reactivity.ts
@@ -0,0 +1,27 @@
+constructor(
+  private readonly pessoaService: PessoaService,
+  private readonly cdr: ChangeDetectorRef // Injeção necessária
+) {}
+
+ngOnInit(): void {
+  this.pessoaService.buscarPorId(1).subscribe((pessoa) => {
+    this.texto = `Nome: ${pessoa.nome}`;
+    this.cdr.markForCheck();
+  });
+
+  setInterval(() => {
+    this.contador++;
+    this.cdr.markForCheck();
+  }, 1000);
+
+  this.pessoaService.buscarPorId(pessoaId).pipe(
+    switchMap(pessoa =>
+      this.pessoaService.buscarQuantidadeFamiliares(pessoa.id).pipe(
+        map(qtd => ({ nome: pessoa.nome, qtd }))
+      )
+    ),
+    takeUntilDestroyed()
+  ).subscribe(res => {
+    this.texto = `Nome: ${res.nome} | familiares: ${res.qtd}`;
+  });
+}
