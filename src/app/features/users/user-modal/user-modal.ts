2678def594469f98bb78cc078ae2709f97ce4cd3
Guilherme Henry <guilherme.henrydesigner@gmail.com>
2026-04-14 15:19:38 -0300

    refactor: remove unused user-modal files
    - Remove user-modal component files as Material was replaced with plain HTML
    - Simplify user management to use prompt/confirm dialogs

--- src/app/features/users/user-modal/user-modal.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-user-modal',
-  imports: [],
-  templateUrl: './user-modal.html',
-  styles: ``,
-})
-export class UserModal {}
