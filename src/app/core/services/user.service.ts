8c1cbd9fffb4e5058f2d439e3d362633e7d9b9aa
Guilherme Henry <guilherme.henrydesigner@gmail.com>
2026-04-14 15:03:36 -0300

    feat: implement user service with signals and loading state

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,24 +1,26 @@
-import { inject, Injectable, signal } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { catchError, finalize, tap } from "rxjs";
-import { of } from "rxjs";
-import { User } from "./user";
+import { Injectable, signal, computed } from '@angular/core';
+import { User } from '../models/user.model';
+import { delay, of, tap, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-  private http = inject(HttpClient);
-  private usersSignal = signal<User[]>([]);
+  private usersState = signal<User[]>([
+    { id: 1, name: 'Giana Sandrini', email: 'giana@attornatus.com.br', cpf: '000.000.000-00', phone: '47999999999', phoneType: 'CELULAR' }
+  ]);
   
-  
-  users = this.usersSignal.asReadonly();
+  users = this.usersState.asReadonly();
   loading = signal(false);
 
-  loadUsers(query: string = '') {
+  loadUsers(query: string = ''): Observable<User[]> {
     this.loading.set(true);
-    return this.http.get<User[]>(`API_URL?q=${query}`).pipe(
-      tap(data => this.usersSignal.set(data)),
-      catchError(err => { return of([]); }),
-      finalize(() => this.loading.set(false))
+    
+    const filtered = this.usersState().filter(u => 
+      u.name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    return of(filtered).pipe(
+      delay(500), 
+      tap(() => this.loading.set(false))
     );
   }
 }
